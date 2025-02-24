a213c54e1ea7e1d57346f4998d0129bf4c4f8e5c
LeonardoAlmds <leonardoalmd8@outlook.com>
2025-02-24 15:17:25 -0300

    FormAbout refactor

--- src/components/AbouteComponents/FormAboutMe/FormAbout.jsx
+++ src/components/AbouteComponents/FormAboutMe/FormAbout.jsx
@@ -1,86 +1,183 @@
 import { useState } from "react";
 import "./FormAbout.css";
 
-const groupedFields = [
-  [{ label: "Nome completo (Nome Social)", name: "name", type: "text" }],
-  [
-    { label: "Email", name: "email", type: "email" },
-    { label: "Email de Contato", name: "contactEmail", type: "email" },
-  ],
-  [
-    { label: "Link do Github", name: "github", type: "text" },
-    { label: "Telefone", name: "phone", isPhone: true },
-  ],
-  [
-    { label: "Data de nascimento", name: "date", type: "date" },
-    { label: "Link do Linkedin", name: "linkedin", type: "text"},
-    { label: "Currículo em PDF", name: "cv", type: "file" },
-  ]
-];
-
-const FormAbout = ({ userData, isOwner }) => {
+const FormAbout = ({ isOwner }) => {
   const [isEditing, setIsEditing] = useState(false);
-  const [formData, setFormData] = useState(userData || {});
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    secondEmail: "",
+    github: "",
+    phone1: "",
+    phone2: "",
+    birthDate: "",
+    linkedin: "",
+    cv: "",
+    pcd: "",
+  });
 
-  const handleEditClick = () => setIsEditing(!isEditing);
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
   };
 
   return (
-    <div className="about">
-      <h1>Informações Pessoais</h1>
+    <div className="about-user">
+      <h1>Informações pessoais</h1>
 
       <div className="form-about">
-        {groupedFields.map((group, index) => (
-          <div key={index} className="field-group">
-            {group.map(({ label, name, type, isPhone }) =>
-              isPhone ? (
-                <div key={name} className="field phone">
-                  <label>{isEditing ? "Telefone*" : "Telefone"}</label>
-                  {isEditing ? (
-                    <div className="phone-inputs">
-                      <input
-                        type="text"
-                        name="ddd"
-                        placeholder="DDD"
-                        value={formData.ddd || ""}
-                        onChange={handleChange}
-                      />
-                      <input
-                        type="text"
-                        name="phone"
-                        placeholder="Número"
-                        value={formData.phone || ""}
-                        onChange={handleChange}
-                      />
-                    </div>
-                  ) : (
-                    <p>{formData.ddd || "Vazio"} - {formData.phone || "Não Informado"}</p>
-                  )}
-                </div>
-              ) : (
-                <div key={name} className="field">
-                  <label>{isEditing ? `${label}*` : label}</label>
-                  {isEditing ? (
-                    <input
-                      type={type}
-                      name={name}
-                      value={formData[name] || ""}
-                      onChange={handleChange}
-                    />
-                  ) : (
-                    <p>{formData[name] || "Não informado"}</p>
-                  )}
-                </div>
-              )
-            )}
+        {/* Nome */}
+        <div className="about-info">
+          <div>
+            <label htmlFor="name">
+                Nome completo (Nome social) {isEditing && <span>*</span>}
+              </label>
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleInputChange}
+                placeholder={formData.name || "Nenhuma Informação"}
+                disabled={!isEditing}
+              />
           </div>
-        ))}
-      </div>
+        </div>
 
-      {isOwner && <button onClick={handleEditClick}>{isEditing ? "Salvar" : "Editar"}</button>}
+        {/* Emails */}
+        <div className="about-info">
+          <div>
+            <label htmlFor="email">
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              placeholder={formData.email || "Nenhuma Informação"}
+              disabled
+            />
+          </div>
+          <div>
+            <label htmlFor="second-email">Email de Contato {isEditing && <span>*</span>}</label>
+            <input
+              type="email"
+              name="secondEmail"
+              value={formData.secondEmail}
+              onChange={handleInputChange}
+              placeholder={formData.secondEmail || "Nenhuma Informação"}
+              disabled={!isEditing}
+            />
+          </div>
+        </div>
+
+        {/* Github e Telefone */}
+        <div className="about-info">
+          <div>
+            <label htmlFor="github">Link do Github <span className="optional">Opcional</span></label>
+            <input
+              type="text"
+              name="github"
+              value={formData.github}
+              onChange={handleInputChange}
+              placeholder={formData.github || "Nenhuma Informação"}
+              disabled={!isEditing}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="phone">Telefone {isEditing && <span>*</span>}</label>
+            <div>
+              <input
+                type="text"
+                name="phone1"
+                value={formData.phone1}
+                onChange={handleInputChange}
+                placeholder={formData.phone1 || "DDD"}
+                disabled={!isEditing}
+              />
+              <input
+                type="text"
+                name="phone2"
+                value={formData.phone2}
+                onChange={handleInputChange}
+                placeholder={formData.phone2 || "Número"}
+                disabled={!isEditing}
+              />
+            </div>
+          </div>
+        </div>
+
+        {/* Data de nascimento, LinkedIn e CV */}
+        <div className="about-info grid-row-about">
+          <div>
+            <label htmlFor="birthDate">Data de nascimento {isEditing && <span>*</span>}</label>
+            <input
+              type="date"
+              name="birthDate"
+              value={formData.birthDate}
+              onChange={handleInputChange}
+              disabled={!isEditing}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="linkedin">Link do Linkedin <span className="optional">Opcional</span></label>
+            <input
+              type="text"
+              name="linkedin"
+              value={formData.linkedin}
+              onChange={handleInputChange}
+              placeholder={formData.linkedin || "Nenhuma Informação"}
+              disabled={!isEditing}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="cv">Currículo em PDF {isEditing && <span>*</span>}</label>
+            <input
+              type="file"
+              name="cv"
+              disabled={!isEditing}
+            />
+          </div>
+        </div>
+
+        {/* Deficiência e botões */}
+        <div className="line-end about-info">
+          <div>
+            <label htmlFor="pcd">Possuo alguma deficiência? {isEditing && <span>*</span>}</label>
+            <div className="about-radio">
+              <input
+                type="radio"
+                name="pcd"
+                value="sim"
+                checked={formData.pcd === "sim"}
+                onChange={handleInputChange}
+                disabled={!isEditing}
+              /> Sim
+              <input
+                type="radio"
+                name="pcd"
+                value="nao"
+                checked={formData.pcd === "nao"}
+                onChange={handleInputChange}
+                disabled={!isEditing}
+                
+              /> Não
+            </div>
+          </div>
+          {isOwner && (
+            <div className="about-button">
+              {isEditing ? (
+                <button onClick={() => setIsEditing(!isEditing)}>Salvar</button>
+              ) : (
+                <button onClick={() => setIsEditing(!isEditing)}>Editar</button>
+              )}
+            </div>
+          )}
+        </div>     
+      </div>
     </div>
   );
 };
