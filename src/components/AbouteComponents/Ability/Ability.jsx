bdbe2dd7946542180bb189d6843f81b925c8955d
LeonardoAlmds <leonardoalmd8@outlook.com>
2025-03-26 16:59:12 -0300

    skill component initialized

--- src/components/AbouteComponents/Ability/Ability.jsx
+++ src/components/AbouteComponents/Ability/Ability.jsx
@@ -7,6 +7,54 @@ const Ability = ({ isOwner }) => {
         <h1>Habilidades</h1>
         <span>Adicione uma nova habilidade</span>
       </div>
+
+      <div className="ability-content">
+        <input type="text" className='search-ability' />
+
+        <div className='ability-info'>
+          <span>Lorem Impsum</span>
+
+          <div className='ability-level'>
+            <div>
+              <input type="radio" name="ability-level" id="ability-level" />
+              <label htmlFor="">Básico</label>
+            </div>
+
+            <div>
+              <input type="radio" name="ability-level" id="ability-level" />
+              <label htmlFor="">Intermediário</label>
+            </div>
+
+            <div>
+              <input type="radio" name="ability-level" id="ability-level" />
+              <label htmlFor="">Avançado</label> 
+            </div>    
+          </div>
+        </div>
+
+        <div className='ability-info'>
+          <span>Lorem Impsum</span>
+
+          <div className='ability-level'>
+            <div>
+              <input type="radio" name="ability-level" id="ability-level" />
+              <label htmlFor="">Básico</label>
+            </div>
+
+            <div>
+              <input type="radio" name="ability-level" id="ability-level" />
+              <label htmlFor="">Intermediário</label>
+            </div>
+
+            <div>
+              <input type="radio" name="ability-level" id="ability-level" />
+              <label htmlFor="">Avançado</label> 
+            </div>    
+          </div>
+        </div>
+
+      </div>
+      
     </div>
   )
 }
